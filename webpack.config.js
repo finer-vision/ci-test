0a67c6e74d107739f09661f531dbb05aac267267
James Craig <jamescraig@finervision.com>
2019-10-22 09:31:59 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const BUILD_PATH = path.resolve(__dirname, 'public', 'build');
+
+const config = {
+    entry: path.join(SRC_PATH, 'index.js'),
+    output: {
+        path: BUILD_PATH,
+        filename: '[name].[contenthash].js',
+        publicPath: '/build/',
+    },
+    plugins: [
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(SRC_PATH, 'index.html'),
+            favicon: path.join(SRC_PATH, 'assets', 'icons', 'favicon.ico'),
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+            },
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\\/]node_modules[\\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif|mp4|woff|woff2|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: BUILD_PATH,
+        writeToDisk: true,
+        port: process.env.DEV_SERVER_PORT || 8081,
+        host: '0.0.0.0',
+        open: false,
+        proxy: {
+            '/': 'http://nginx',
+        },
+    },
+};
+
+module.exports = (env, argv) => {
+    if (argv.hot) {
+        // contenthash isn't available when hot reloading.
+        config.output.filename = '[name].[hash].js';
+    }
+
+    return config;
+};
