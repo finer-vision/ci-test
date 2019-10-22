b2fe84403ac37abde4ffb6beb3e984c9d5cdaa32
James Craig <jamescraig@finervision.com>
2019-10-22 18:27:03 +0100

    CI Test 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                CI Test 1
+                CI Test 2
             </div>
         );
     }
